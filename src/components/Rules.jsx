1e1492453c2978c06d1e8f939125bdebe7d4a44a
vergarapog <201801166@iacademy.edu.ph>
2022-12-10 20:11:14 +0800

    finished: rules button component finished fe

---
+++ src/components/Rules.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Rules = () => {
+  return (
+    <section className="flex justify-center items-center">
+      <div className="border-gray-400 border-2 py-3 px-8 rounded-xl">
+        <h1 className="text-white font-barlow font-semibold tracking-[0.2em]">
+          RULES
+        </h1>
+      </div>
+    </section>
+  )
+}
+
+export default Rules
