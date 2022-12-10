aad4f355f456acb97d39fd11e9a6a52ee3d8b8f9
vergarapog <201801166@iacademy.edu.ph>
2022-12-10 19:39:13 +0800

    added: inset shadows to Play Icons

--- src/components/PlayIcon.jsx
+++ src/components/PlayIcon.jsx
@@ -3,9 +3,9 @@ import React from "react"
 const PlayIcon = ({ icon, coordinates, gradient }) => {
   return (
     <div
-      className={`absolute ${coordinates} w-28 h-28 bg-white bg-gradient-to-b ${gradient}  rounded-full flex justify-center items-center`}
+      className={`absolute ${coordinates} w-28 h-28 bg-white bg-gradient-to-b ${gradient}  rounded-full flex justify-center items-center shadow-[inset_0_-0.3rem_rgba(0,0,0,0.2)]`}
     >
-      <div className="bg-white w-[5.5rem] h-[5.5rem] flex justify-center items-center rounded-full">
+      <div className="bg-white w-[5.5rem] h-[5.5rem] flex justify-center items-center rounded-full shadow-[inset_0_0.3rem_rgba(0,0,0,0.2)]">
         <img src={icon} alt="" className="w-11" />
       </div>
     </div>
