2c6c0a6cf9c62f90bb2ebc5013c1eb68ad21f81a
vergarapog <201801166@iacademy.edu.ph>
2022-12-27 18:37:47 +0800

    fixed: rock paper logo size and scoreboard size on desktop screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,14 +9,14 @@ const Header = () => {
     <section className="border-headerOutline border-4 rounded lg:rounded-2xl">
       <div className="h-full flex justify-between items-center p-4">
         <div>
-          <img src={logo} className="w-24" alt="" />
+          <img src={logo} className="w-24 lg:w-48 " alt="" />
         </div>
-        <div className="bg-white w-24 h-20 rounded flex items-center justify-center">
+        <div className="bg-white w-24 h-20 lg:w-32 lg:h-28 rounded flex items-center justify-center">
           <div className="my-auto space-y-0 text-center">
-            <h3 className="text-scoreText text-xs font-barlow font-bold tracking-widest">
+            <h3 className="text-scoreText text-xs lg:text-base font-barlow font-bold tracking-widest">
               SCORE
             </h3>
-            <h1 className="text-darkText text-4xl font-barlow font-extrabold">
+            <h1 className="text-darkText text-4xl lg:text-7xl font-barlow font-extrabold">
               {score}
             </h1>
           </div>
